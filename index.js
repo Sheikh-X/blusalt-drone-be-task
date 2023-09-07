bb0b405dd665df90b41588e15c7fc01f327072c4
afeez.bakare <afeez.bakare@ivantage.africa>
2023-09-07 16:54:44 +0100

    added the endpoints to add a new drone and get all drones

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const port = 3005;
 const db = new sqlite3.Database(":memory:"); //memory option picked as I'm not persisiting data.
 
 //queries to create  the medication and drone tables
-db.run(`
-    CREATE TABLE Drone (
+db.serialize(() => {
+  //using serialize to ensure the queries run sequentially as defined below
+  db.run(`
+    CREATE TABLE Drones (
       serialNumber TEXT PRIMARY KEY CHECK(length(serialNumber) <= 100),
       model TEXT CHECK(model IN ('Lightweight', 'Middleweight', 'Cruiserweight', 'Heavyweight')),
       weightLimit REAL CHECK(weightLimit <= 500),
@@ -16,7 +18,7 @@ db.run(`
     )
   `);
 
-db.run(`
+  db.run(`
     CREATE TABLE Medication (
       name TEXT PRIMARY KEY CHECK(name GLOB '[A-Za-z0-9_-]*'),
       weight REAL,
@@ -24,10 +26,53 @@ db.run(`
       image BLOB 
     )
   `);
+});
 //for a production app image will be saved in a storage like s3bucket or azure blob storage with the url and key be saved to the database
 //instead of storing it as blob like I have doen above.
 
 app.use(express.json());
+app.post("/drones", (req, res) => {
+  const { serial_number, model, weight_limit, battery_capacity, state } =
+    req.body;
+  if (
+    !(
+      req.body.serial_number &&
+      req.body.model &&
+      req.body.weight_limit &&
+      req.body.battery_capacity &&
+      req.body.state
+    )
+  ) {
+    return res.status(400).json({ error: "All parameters are required" });
+  }
+
+  db.run(
+    `INSERT INTO Drones (serialNumber, model, weightLimit, batteryCapacity, state)
+    VALUES (?, ?, ?, ?, ?)`,
+    [serial_number, model, weight_limit, battery_capacity, state],
+    (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Failed to register the drone" });
+      }
+      res.json({ message: "Drone registered successfully" });
+    }
+  );
+});
+app.get("/drones/all", (req, res) => {
+  db.all(
+    `SELECT *
+    FROM Drones`,
+    (err, rows) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Failed to fetch drones" });
+      }
+      res.json(rows);
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
